dd26d4d6fc1bc981dd4b287b9f062b380040ba89
aminesayagh <aminesayagh1997@gmail.com>
2020-11-30 17:10:36 +0100

    store

--- store/index.js
+++ store/index.js
@@ -1,7 +1,39 @@
-import scElementMenu from './scElementMenu'
+export const state = () => ({
+      
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import scElementMenu from './scElementMenu'
 
 // Vue.use(Vuex)
 
-export const state = () => ({
-    scElementMenu,
-  })
+// export const state = () => ({
+//     modules: {
+//         state: () => ({
+            
+//         }),
+        
+//     }
+    
+//   })
+
+//   new Vuex.Store({
+//     state: () => ({
+//         scElementMenu
+//     }),
+//   })
+
